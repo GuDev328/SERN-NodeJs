12fd976ff421bc16d7ae731b0eb3abd6ed8d3da1
datgu <datminiphi@gmail.com>
2023-02-20 02:00:22 +0700

    complete service send email booking

---
+++ src/services/emailService.js
@@ -0,0 +1,78 @@
+var db = require('../models/index')
+const nodemailer = require("nodemailer");
+import moment from 'moment'
+require('dotenv').config()
+
+let sendEmailComfirmBookingAppointment = async (sendData) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_APP, // generated ethereal user
+            pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+        },
+    });
+    if (sendData.patientGender) {
+        if (sendData.language === 'vi') {
+            if (sendData.patientGender === 'M') sendData.patientGender = 'Nam'
+            if (sendData.patientGender === 'F') sendData.patientGender = 'Nữ'
+            if (sendData.patientGender === 'O') sendData.patientGender = 'Khác'
+        } else {
+            if (sendData.patientGender === 'M') sendData.patientGender = 'Male'
+            if (sendData.patientGender === 'F') sendData.patientGender = 'Female'
+            if (sendData.patientGender === 'O') sendData.patientGender = 'Other'
+        }
+    }
+    let htmlVi = `
+    <h3>Xin chào ${sendData.patientName}</h3>
+    <p>Bạn nhận được email này vì đã đặt lịch khám bệnh trên BookingCareClone.</p>
+    <p>Thông tin đặt lịch: </p>
+    <p><b>Bác sĩ: </b> ${sendData.doctorName}</p>
+    <p><b>Thời gian: </b> ${sendData.time}  -  ${sendData.date}</p>
+    <p><b>Giá khám: </b> ${sendData.price.valueVi}</p>
+    <p></p>
+    <p><b>Họ tên bệnh nhân: </b> ${sendData.patientName}</p>
+    <p><b>Giới tính: </b> ${sendData.patientGender}</p>
+    <p><b>Số điện thoại: </b> ${sendData.patientPhoneNumber}</p>
+    <p><b>Ngày sinh: </b> ${sendData.patientDob}</p>
+    <p><b>Địa chỉ: </b> ${sendData.patientAddress}</p>
+    <p><b>Lí do khám bệnh: </b> ${sendData.reason}</p>
+    <p>Nếu các thông tin trên là đúng, mời bạn vui lòng bấm vào đường link bên dưới để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh.</p>
+    <a href='https://www.youtube.com/'>Bấm và đây</a>
+    <h3>Xin chân thành cảm ơn!</h3>`
+
+    let htmlEn = `
+    <h3>Dear, ${sendData.patientName}</h3>
+    <p>You received this email because you booked a medical appointment on BookingCareClone.</p>
+    <p>Booking information: </p>
+    <p><b>Doctor: </b> ${sendData.doctorName}</p>
+    <p><b>Time: </b> ${sendData.time}  -  ${sendData.date}</p>
+    <p><b>Cost: </b> ${sendData.price.valueEn}</p>
+    <p></p>
+    <p><b>Patient name: </b> ${sendData.patientName}</p>
+    <p><b>Gender: </b> ${sendData.patientGender}</p>
+    <p><b>Phone number: </b> ${sendData.patientPhoneNumber}</p>
+    <p><b>DOB: </b> ${sendData.patientDob}</p>
+    <p><b>Address: </b> ${sendData.patientAddress}</p>
+    <p><b>Reasons for medical examination: </b> ${sendData.reason}</p>
+    <p>If the above information is correct, please click on the link below to confirm and complete the medical appointment booking procedure.</p>
+    <a href='https://www.youtube.com/'>Click here</a>
+    <h3>Sincerely thank!</h3>
+    <h3>----------------</h3>
+    <h3>BookingCareClone</h3>`
+    let htmlContent = sendData.language === 'vi' ? htmlVi : htmlEn
+    let subjectContent = sendData.language === 'vi' ? "Xác nhận đặt lịch khám bệnh tại BookingCare" : "Confirm your appointment at BookingCare"
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"BookingCareCloneByGuDev" <foo@example.com>', // sender address
+        to: sendData.receiverEmail, // list of receivers
+        subject: subjectContent, // Subject line
+        html: htmlContent // html body
+    });
+}
+
+module.exports = {
+    sendEmailComfirmBookingAppointment: sendEmailComfirmBookingAppointment
+}
--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,10 +1,10 @@
 var db = require('../models/index')
+var emailService = require('../services/emailService')
 import moment from 'moment'
 
 let postBooking = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            console.log(data)
             if (data && data.doctorId
                 && data.patientId
                 && data.timeType
@@ -24,10 +24,18 @@ let postBooking = (data) => {
                     }
                 })
                 if (bookingCheck) {
-                    resolve({
-                        errCode: 2,
-                        errMessage: 'This appointment has already been booked, Unable to rebook'
-                    })
+                    if (bookingCheck.statusId === 'S2') {
+                        resolve({
+                            errCode: 5,
+                            errMessage: 'This appointment has already been booked, Unable to rebook'
+                        })
+                    }
+                    if (bookingCheck.statusId === 'S1') {
+                        resolve({
+                            errCode: 2,
+                            errMessage: 'This appointment has already been booked, Please comfirm this appointment in your email'
+                        })
+                    }
                 } else {
                     let schedule = await db.Schedule.findOne({
                         where: {
@@ -73,15 +81,58 @@ let postBooking = (data) => {
                                     address: data.patientAddress,
                                     reason: data.patientReason,
                                 })
+                                let userPatient = await db.User.findOne({
+                                    where: {
+                                        id: data.patientId,
+                                    }
+                                })
+                                let userDoctor = await db.User.findOne({
+                                    where: {
+                                        id: data.doctorId,
+                                    },
+                                    include: [
+                                        {
+                                            model: db.DoctorInfo,
+                                            include: [
+                                                { model: db.Allcode, as: 'priceData', attributes: ['valueEn', 'valueVi'] },
+                                            ]
+                                        },
+                                    ],
+                                    raw: true,
+                                    nest: true,
+                                })
+                                let doctorName = data.language === 'vi' ? userDoctor.lastName + ' ' + userDoctor.firstName : userDoctor.firstName + ' ' + userDoctor.lastName
+                                await emailService.sendEmailComfirmBookingAppointment({
+                                    receiverEmail: userPatient.email,
+                                    doctorName: doctorName,
+                                    time: data.timeData.valueVi,
+                                    date: data.date,
+                                    patientName: data.patientName,
+                                    patientGender: data.patientGender,
+                                    patientDob: data.patientDob,
+                                    patientPhoneNumber: data.patientPhoneNumber,
+                                    patientAddress: data.patientAddress,
+                                    reason: data.patientReason,
+                                    price: userDoctor.DoctorInfo.priceData,
+                                    language: data.language
+                                })
                                 resolve({
                                     errCode: 0,
                                     errMessage: 'Successful appointment'
                                 })
                             } else {
-                                resolve({
-                                    errCode: 2,
-                                    errMessage: 'This appointment has already been booked, Unable to rebook'
-                                })
+                                if (booking.statusId === 'S2') {
+                                    resolve({
+                                        errCode: 5,
+                                        errMessage: 'This appointment has already been booked, Unable to rebook'
+                                    })
+                                }
+                                if (booking.statusId === 'S1') {
+                                    resolve({
+                                        errCode: 2,
+                                        errMessage: 'This appointment has already been booked, Please comfirm this appointment in your email'
+                                    })
+                                }
                             }
                         });
                     }
